699e8bf44a1642327573b5c07e49e87dc4ec31b5
Kamal-Abdullayev <kamalabdullayevv@gmail.com>
2022-06-20 22:12:33 +0400

    feat: Front

---
+++ TestPro2/Model/Slider.cs
@@ -0,0 +1,10 @@
+﻿namespace TestPro2.Model
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
